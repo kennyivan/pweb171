99a84626f5be99856cf181c8efa065c0f98464ea
kennyivan <32442480+kennyivan@users.noreply.github.com>
2018-03-01 16:28:20 +0800

    Complete Commit
    Title lupa ganti

--- blog171/application/views/layout/header.php
+++ blog171/application/views/layout/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Anyar-HTML Bootstrap theme</title>
+  <title>Xianxia World</title>
 
   <!-- Bootstrap -->
   <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
